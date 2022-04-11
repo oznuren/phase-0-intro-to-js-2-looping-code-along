f18fb569bfd5593a1d5e4b1157100ded7fe12329
Öznur <oznur@tuta.io>
2022-04-11 23:37:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+let names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names, eventName) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let str = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(str);
+  }
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
